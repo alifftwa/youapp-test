b607ffd3598933129edc666818bbb63c694ef755
labit-riset-umm <71822965+alifftwa@users.noreply.github.com>
2024-01-24 21:23:31 +0700

    feat: create login page

--- src/app/_modules/login/pages/LoginPage.tsx
+++ src/app/_modules/login/pages/LoginPage.tsx
@@ -1,13 +1,58 @@
-"use client"
+"use client";
 
-import React from 'react'
+import { ArrowBackIos } from "@mui/icons-material";
+import React from "react";
 
 const LoginPage = () => {
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <div className="w-full h-screen overflow-hidden bg-cover bg-gradient-to-bl from-[#1F4247] from-[0%] via-[#0D1D23] via-[56.18%] to-[#09141A] to-[100%] ">
+      <div className="flex pt-5 pl-3">
+        <ArrowBackIos />
+        <p className="font-semibold">Back</p>
+      </div>
 
-export default LoginPage
+      <div className="flex flex-col h-screen justify-center">
+        <div className="mx-6 my-5 align-middle">
+          <div className="mb-5 ml-3">
+            <h1 className="text-2xl font-semibold">Login</h1>
+          </div>
+
+          <div className="my-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Enter Username/Email"
+            />
+          </div>
+          <div className="my-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Enter Password"
+            />
+          </div>
+
+          <div>
+            <button className="w-full py-2 px-4 font-bold rounded-lg text-white bg-gradient-to-r from-[#62CDCB] from-[24.88%] to-[#4599DB] to-[78.49%]">
+              Login
+            </button>
+          </div>
+          <div>
+            <div className="flex justify-center mt-5">
+              <p className="">No account?</p>
+              <button className="pl-1 underline underline-offset-3">
+                <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#F3EDA6] from-[-6.8%] via-[#F8FAE5] via-[89.01%] to-[#D5BE88] to-[100.43%]">
+                  Register here
+                </p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
