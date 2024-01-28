0ce9397ca3b8b946165131177b6db3b63558169e
labit-riset-umm <71822965+alifftwa@users.noreply.github.com>
2024-01-28 13:22:40 +0700

    feat: fix navigation, fix dashboard username

--- src/app/_modules/dashboard/pages/about/AboutEditPage.tsx
+++ src/app/_modules/dashboard/pages/about/AboutEditPage.tsx
@@ -1,15 +1,38 @@
 import Card from "@/app/_components/fragments/card/Card";
 import Form from "@/app/_components/form/Form";
 import { ArrowBackIos } from "@mui/icons-material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Router from "next/router";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const AboutEditPage = () => {
   const pathname = usePathname();
   const finalSlashIndex = pathname.lastIndexOf("/");
   const previousPathname = pathname.slice(0, finalSlashIndex);
+
+  const { data: session, status } = useSession();
+  console.log(session);
+  // if (status === "loading") {
+  //   return <p>Please wait...</p>;
+  // }
+
+  const [storedName, setStoredName] = useState("");
+
+  useEffect(() => {
+    // Check if the user is authenticated and has a name
+    if (status === "authenticated" && session?.user?.name) {
+      // Store the user's name in local storage
+      localStorage.setItem("user_name", session.user.name);
+      setStoredName(session.user.name);
+    } else {
+      // If not authenticated, clear the stored name
+      localStorage.removeItem("user_name");
+      setStoredName("");
+    }
+  }, [session, status]);
+
   return (
     <>
       <div className="h-full overflow-hidden bg-[#09141A]">
@@ -18,14 +41,20 @@ const AboutEditPage = () => {
           <div className="relative flex mb-5 pt-5 pl-3">
             <ArrowBackIos />
             <label className="font-semibold">Back</label>
-            <p className="absolute font-semibold pl-1 left-36">@username</p>
+            <p className="absolute font-semibold pl-1 left-36">
+              {" "}
+              {status === "authenticated" ? "@" + session.user?.name : status}
+            </p>
           </div>
         </Link>
 
         <div className="mx-3">
           <div className="relative w-full h-48 mb-8 overflow-hidden rounded-xl bg-[#162329]">
             <div className="absolute bottom-3 left-3 font-semibold">
-              <p>@username</p>
+              <p>
+                {" "}
+                {status === "authenticated" ? "@" + session.user?.name : status}
+              </p>
             </div>
           </div>
 
@@ -78,7 +107,7 @@ const AboutEditPage = () => {
 
           <Card
             CardName="Interest"
-            Nav="/interest"
+            Nav="/dahsboard/interest"
             Text="Add in your interest to find a better match"
           />
         </div>
